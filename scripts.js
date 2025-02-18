09c9dd846b352161aa17901c2c8f4c76d82f292a
Sebastian Avila <134023565+Sebastian1996avila@users.noreply.github.com>
2025-02-18 12:23:49 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,141 +1,126 @@
-/* Credit and Thanks:
-Matrix - Particles.js;
-SliderJS - Ettrics;
-Design - Sara Mazal Web;
-Fonts - Google Fonts
-*/
-
+// Inicialización de Particles.js para el fondo
 window.onload = function () {
-    Particles.init({
+  Particles.init({
       selector: ".background"
-    });
-  };
-  const particles = Particles.init({
-    selector: ".background",
-    color: ["#03dac6", "#ff0266", "#000000"],
-    connectParticles: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        options: {
-          color: ["#faebd7", "#03dac6", "#ff0266"],
-          maxParticles: 43,
-          connectParticles: false
-        }
-      }
-    ]
   });
-  
-  class NavigationPage {
-    constructor() {
+};
+
+// Inicialización de Particles.js con colores y configuraciones adicionales
+const particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+      {
+          breakpoint: 768,
+          options: {
+              color: ["#faebd7", "#03dac6", "#ff0266"],
+              maxParticles: 43,
+              connectParticles: false
+          }
+      }
+  ]
+});
+
+// Clase de navegación para manejar las pestañas y desplazamiento
+class NavigationPage {
+  constructor() {
       this.currentId = null;
       this.currentTab = null;
       this.tabContainerHeight = 70;
       this.lastScroll = 0;
       let self = this;
       $(".nav-tab").click(function () {
-        self.onTabClick(event, $(this));
+          self.onTabClick(event, $(this));
       });
       $(window).scroll(() => {
-        this.onScroll();
+          this.onScroll();
       });
       $(window).resize(() => {
-        this.onResize();
+          this.onResize();
       });
-    }
-  
-    onTabClick(event, element) {
+  }
+
+  onTabClick(event, element) {
       event.preventDefault();
-      let scrollTop =
-        $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+      let scrollTop = $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
       $("html, body").animate({ scrollTop: scrollTop }, 600);
-    }
-  
-    onScroll() {
+  }
+
+  onScroll() {
       this.checkHeaderPosition();
       this.findCurrentTabSelector();
       this.lastScroll = $(window).scrollTop();
-    }
-  
-    onResize() {
+  }
+
+  onResize() {
       if (this.currentId) {
-        this.setSliderCss();
+          this.setSliderCss();
       }
-    }
-  
-    checkHeaderPosition() {
+  }
+
+  checkHeaderPosition() {
       const headerHeight = 75;
       if ($(window).scrollTop() > headerHeight) {
-        $(".nav-container").addClass("nav-container--scrolled");
+          $(".nav-container").addClass("nav-container--scrolled");
       } else {
-        $(".nav-container").removeClass("nav-container--scrolled");
+          $(".nav-container").removeClass("nav-container--scrolled");
       }
-      let offset =
-        $(".nav").offset().top +
-        $(".nav").height() -
-        this.tabContainerHeight -
-        headerHeight;
+      let offset = $(".nav").offset().top + $(".nav").height() - this.tabContainerHeight - headerHeight;
       if (
-        $(window).scrollTop() > this.lastScroll &&
-        $(window).scrollTop() > offset
+          $(window).scrollTop() > this.lastScroll &&
+          $(window).scrollTop() > offset
       ) {
-        $(".nav-container").addClass("nav-container--move-up");
-        $(".nav-container").removeClass("nav-container--top-first");
-        $(".nav-container").addClass("nav-container--top-second");
+          $(".nav-container").addClass("nav-container--move-up");
+          $(".nav-container").removeClass("nav-container--top-first");
+          $(".nav-container").addClass("nav-container--top-second");
       } else if (
-        $(window).scrollTop() < this.lastScroll &&
-        $(window).scrollTop() > offset
+          $(window).scrollTop() < this.lastScroll &&
+          $(window).scrollTop() > offset
       ) {
-        $(".nav-container").removeClass("nav-container--move-up");
-        $(".nav-container").removeClass("nav-container--top-second");
-        $(".nav-container-container").addClass("nav-container--top-first");
+          $(".nav-container").removeClass("nav-container--move-up");
+          $(".nav-container").removeClass("nav-container--top-second");
+          $(".nav-container-container").addClass("nav-container--top-first");
       } else {
-        $(".nav-container").removeClass("nav-container--move-up");
-        $(".nav-container").removeClass("nav-container--top-first");
-        $(".nav-container").removeClass("nav-container--top-second");
+          $(".nav-container").removeClass("nav-container--move-up");
+          $(".nav-container").removeClass("nav-container--top-first");
+          $(".nav-container").removeClass("nav-container--top-second");
       }
-    }
-  
-    findCurrentTabSelector(element) {
+  }
+
+  findCurrentTabSelector(element) {
       let newCurrentId;
       let newCurrentTab;
       let self = this;
       $(".nav-tab").each(function () {
-        let id = $(this).attr("href");
-        let offsetTop = $(id).offset().top - self.tabContainerHeight;
-        let offsetBottom =
-          $(id).offset().top + $(id).height() - self.tabContainerHeight;
-        if (
-          $(window).scrollTop() > offsetTop &&
-          $(window).scrollTop() < offsetBottom
-        ) {
-          newCurrentId = id;
-          newCurrentTab = $(this);
-        }
+          let id = $(this).attr("href");
+          let offsetTop = $(id).offset().top - self.tabContainerHeight;
+          let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+          if (
+              $(window).scrollTop() > offsetTop &&
+              $(window).scrollTop() < offsetBottom
+          ) {
+              newCurrentId = id;
+              newCurrentTab = $(this);
+          }
       });
       if (this.currentId != newCurrentId || this.currentId === null) {
-        this.currentId = newCurrentId;
-        this.currentTab = newCurrentTab;
-        this.setSliderCss();
+          this.currentId = newCurrentId;
+          this.currentTab = newCurrentTab;
+          this.setSliderCss();
       }
-    }
-  
-    setSliderCss() {
+  }
+
+  setSliderCss() {
       let width = 0;
       let left = 0;
       if (this.currentTab) {
-        width = this.currentTab.css("width");
-        left = this.currentTab.offset().left;
+          width = this.currentTab.css("width");
+          left = this.currentTab.offset().left;
       }
       $(".nav-tab-slider").css("width", width);
       $(".nav-tab-slider").css("left", left);
-    }
   }
-  
-  new NavigationPage();
-  /* Credit and Thanks:
-  Matrix - Particles.js;
-  SliderJS - Ettrics;
-  Design - Sara Mazal Web;
-  Fonts - Google Fonts
-  */
+}
+
+new NavigationPage();
